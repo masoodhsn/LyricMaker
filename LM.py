1f75f24183aac2e0d0d3cb83f5ba60ca2419219d
masoud <masood.hsn82@gmail.com>
2024-09-14 10:17:03 +0330

    convert mp3 to wav

--- LM.py
+++ LM.py
@@ -1,21 +1,29 @@
-﻿from ast import Pass
-import pygame
+﻿import pygame
 import keyboard
 import os
 import glob
+import shutil  # برای جابجا کردن فایل
+from pydub import AudioSegment
 
 # راه‌اندازی pygame
 pygame.mixer.init()
 
-# پیدا کردن اولین فایل mp3 در دایرکتوری
+# پیدا کردن اولین فایل mp3 در دایرکتوری 
 def find_first_mp3_file():
     mp3_files = glob.glob('*.mp3')
     if mp3_files:
         return mp3_files[0]
+    else:
+        exit(0)
     return None
 
 # پیدا کردن فایل صوتی
 filename = find_first_mp3_file()
+
+shutil.rmtree(filename[0:-4])
+
+# ساخت پوشه با نام آهنگ بدون پسوند
+os.makedirs(filename[0:-4], exist_ok=True)
 
 if filename:
     # بارگذاری فایل صوتی
@@ -25,34 +33,32 @@ else:
     print('No MP3 file found in the directory.')
     exit(0)
 
-
-print('please entre full lyric (press Ctrl+Z to finish):')
+print('Please enter full lyrics (press Ctrl+Z to finish):')
 
 lines = []
+ep = True
+line = ''
 
-ep=True
-line=''
+# دریافت ورودی‌های کاربر برای شعر
 while True:
     try:
         i = input()
         if not i == '':
             if ep:
-                line=i+'<br>'
-                ep=False
+                line = i + '<br>'
+                ep = False
             else:
-                lines.append(line+i)
-                ep=True
-                line=''
+                lines.append(line + i)
+                ep = True
+                line = ''
     except EOFError:
         break
-
 
 # پخش آهنگ
 pygame.mixer.music.play()
 
-
-out=''
-counter=0
+out = ''
+counter = 0
 
 print("Press 'p' to place time on lyric line")
 
@@ -65,29 +71,42 @@ def show_time():
     print(f"Music time: {minutes:02}:{seconds:02} , lines {counter+1}/{len(lines)}")
     return f'[00:{minutes:02}:{seconds:02}]'
 
-
 # حلقه برای گوش دادن به ورودی صفحه کلید
 while True:
     try:
         # اگر کلید p فشار داده شود
         if keyboard.is_pressed('p'):
-            temp=show_time()+lines[counter]+'\n'
-            counter=counter+1
+            temp = show_time() + lines[counter] + '\n'
+            counter = counter + 1
             print(temp)
-            out=out+temp
+            out = out + temp
+            
             
             # برای جلوگیری از چند بار اجرا شدن با یک فشار دکمه، مکث کوتاه اضافه می‌کنیم
             while keyboard.is_pressed('p'):
                 pass
 
         if not (len(lines) - counter):
-            
-            with open(f'{filename[0:-4]}.lrc', 'w' , encoding='utf-8') as file:
+            with open(f'{filename[0:-4]}.lrc', 'w', encoding='utf-8') as file:
                 file.write(out)
-            print("Lyrics saved to music.lrc")
+            print("Lyrics saved to .lrc file")
+
             break
 
-
-    except:
-        print('exeption')
+    except Exception as e:
+        print(f'Exception: {e}')
         break
+
+
+pygame.mixer.music.stop()
+pygame.mixer.music.unload()
+# جابجا کردن فایل به پوشه جدید با استفاده از shutil.move
+
+sound = AudioSegment.from_mp3(filename)
+sound.export(filename[0:-4]+'.wav', format="wav")
+
+
+shutil.move(filename[0:-4]+'.lrc', filename[0:-4])
+shutil.move(filename[0:-4]+'.wav', filename[0:-4])
+
+os.remove(filename)
